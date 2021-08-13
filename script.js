990c25d8c6a4a09b633beaa98d3acec058566269
dim50dim <zheliezkov@gmail.com>
2021-08-13 12:40:10 +0300

    first com

---
+++ script.js
@@ -0,0 +1,54 @@
+//////////////////////////////////////////////////////////////////
+class Student  {
+    constructor (a,b,c,d,e) {
+        this.firstName = a;
+        this.secondName = b;
+        this.dayOfBirth = c;
+        this.arr = [];
+        this.arr2 = [];
+    }
+    present(){
+        this.arr.push(true);
+    }
+    absent(){
+        this.arr.push(false);
+    }
+    mark(num){
+        if(this.arr2.length >= 0 && this.arr2.length <= 10 || this.num >= 0  && this.num <=10) { 
+            this.arr2.push(num);
+        }
+    }
+}
+
+
+////////////////////////////////////////////////////////////////////
+let student = new Student('Alex','Ivanov','11.22.1233',[],[]);
+console.log(student);
+
+student.present();
+student.present();
+student.absent();
+student.absent();
+student.absent();
+student.absent();
+student.mark(8);
+
+let student2 = new Student('Mike','Smith','29.22.1233',[],[]);
+console.log(student2);
+
+student2.present();
+student2.present();
+student2.absent();
+
+student2.absent();
+student2.mark(7);
+
+let student3 = new Student('John','Black','22.44.2354',[],[]);
+console.log(student3);
+
+student3.present();
+student3.present();
+student3.absent();
+
+student3.absent();
+student3.mark(5);
